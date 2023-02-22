8d8b535878a5ea0ac2d56a29daea54b7c8dfc938
SFauth <SFauth@gmx.net>
2023-02-22 17:25:02 +0000

    produces prototype plot with exp results

--- image_captioning/evaluation/cocoeval.py
+++ image_captioning/evaluation/cocoeval.py
@@ -1,6 +1,8 @@
 from pycocotools.coco import COCO
 from pycocoevalcap.eval import COCOEvalCap
 import json
+import os
+import glob
 from json import encoder
 from datetime import datetime
 encoder.FLOAT_REPR = lambda o: format(o, '.3f')
@@ -8,31 +10,41 @@ encoder.FLOAT_REPR = lambda o: format(o, '.3f')
 import argparse
 def parse_config():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--result_file_path", type=str, help="the result file to evaluate")
+    parser.add_argument("--result_file_path", type=str, help="the folder where the result file paths are stored in")
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_config()
-    cocoEval = COCOEvalCap(args.result_file_path)
-    cocoEval.evaluate()
+
+    file_pattern = os.path.join(args.result_file_path, '*.json')
+    result_files = glob.glob(file_pattern)
+
+    result_dicts = []
+
+    for result_file in result_files:
+        # calculate metrics and store 
+        cocoEval = COCOEvalCap(result_file)
+        cocoEval.evaluate()
     # add to cocoEval.eval dict all hyperparams (alpha, beta, prompt, ...). include them in the dict in args.result_file_path
 
-    res_dict = cocoEval.eval
+        res_dict = cocoEval.eval
 
-    all_res_dict = json.load(open(args.result_file_path))
+        all_res_dict = json.load(open(result_file))
 
     # all_res_dict: list of dicts
 
-    keys_to_remove = ["captions", "sound_name", "split", "prediction"]
-    for k in keys_to_remove:
-        del all_res_dict[0][k]
+        keys_to_remove = ["captions", "sound_name", "split", "prediction"]
+        for k in keys_to_remove:
+            del all_res_dict[0][k]
 
-    for hyperparam in all_res_dict[0].keys():
-        res_dict[str(hyperparam)] = all_res_dict[0][str(hyperparam)]
+        for hyperparam in all_res_dict[0].keys():
+            res_dict[str(hyperparam)] = all_res_dict[0][str(hyperparam)]
+        
+        result_dicts.append(res_dict)
 
     filename = "test_" + datetime.today().strftime('%Y_%m_%d_%H_%M_%S') + ".json"
     with open (filename, "w", encoding="utf-8") as f:
-        json.dump(res_dict, f, ensure_ascii=False, indent=4)
+        json.dump(result_dicts, f, ensure_ascii=False, indent=4)
 
     #for metric, score in cocoEval.eval.items():
      #   print ('%s: %.3f'%(metric, score))
--- image_captioning/evaluation/evaluation.sh
+++ image_captioning/evaluation/evaluation.sh
@@ -1,2 +1,2 @@
 python cocoeval.py\
-    --result_file_path ../inference_result/clotho_v2.1/magic/magic_clotho_v2.1_clap_demo.json
+    --result_file_path ../inference_result/clotho_v2.1/magic/hyperparam_experiments/
